303c0eab4b77dd96d9e5994876953390e94887ef
Sungjoon Moon <sumoon@seoulsaram.org>
2026-07-17 22:54:31 +0900

    widen dotum tone mark vmtx range for bold glyph order

--- fix-vmtx.pl
+++ fix-vmtx.pl
@@ -2,8 +2,9 @@
 my $batang = $ARGV[0] =~ /Batang/ ? 1 : 0;
 
 # HCR v2.120: jungseong/jongseong variants uni1161.y0 .. uniD7FB.y4,
-# then tone mark vert/other glyphs appended at the end
-my ($start,$stop,$tmvstart, $tmvstop) = (63274,64629,65206,65211);
+# then tone mark vert/other glyphs at the end; Dotum-Bold interleaves
+# two full-width glyphs there, skipped by the height==0 guard
+my ($start,$stop,$tmvstart, $tmvstop) = (63274,64629,65204,65211);
 if ($batang) {
   $start = 62830;
   $stop = 64185;
